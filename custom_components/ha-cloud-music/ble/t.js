d7dbe83a617ff4ba697c9b98d40dae9376ab252d
zhulu <shaonianzhentan@sina.cn>
2019-10-24 22:14:22 +0800

    解决扫描报错问题

--- custom_components/ha-cloud-music/ble/t.js
+++ custom_components/ha-cloud-music/ble/t.js
@@ -11,18 +11,24 @@ function scan() {
   log('开始扫描蓝牙设备...');
   PythonShell.run('t.py', null, function (err, result) {
 
-    log('扫描完成...');
     if (err) {
       log('出现错误：', err);
     } else {
-      let res = JSON.parse(result[0])
-      if (Array.isArray(res)) {
-        ble_list = res
-        // log(res)
+      try {
+        let res = JSON.parse(result[0])
+        if (Array.isArray(res)) {
+          ble_list = res
+          // log(res)
+          log('扫描完成...');
+        }
+      } catch (ex) {
+        log('出现异常：', ex)
       }
     }
     // 重新搜索
-    scan()
+    setTimeout(() => {
+      scan()
+    }, 1000)
   });
 
 }
@@ -43,6 +49,7 @@ http.createServer(function (req, res) {//回调函数
     res.writeHead(200, { 'Content-Type': 'application/json;charset=UTF-8' })
     res.end(JSON.stringify(ble_list))
   } else if (req.url === '/scan') {
+    scan()
     res.end("重新扫描")
   } else {
     res.end('404')
