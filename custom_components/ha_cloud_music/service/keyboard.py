7325d5e63f288c00f81401ef7d9f667905c824f5
zhulu <shaonianzhentan@sina.cn>
2019-11-12 17:20:41 +0800

    解决监听键盘配置问题

--- custom_components/ha_cloud_music/service/keyboard.py
+++ custom_components/ha_cloud_music/service/keyboard.py
@@ -8,6 +8,8 @@
 
 安装依赖
 pip install evdev
+pip install pyyaml
+pip install requests
 
 运行程序
 python3 keyboard.py
@@ -16,15 +18,26 @@ from evdev import InputDevice
 from select import select
 from requests import post
 import json
+import yaml
+import os
+
+# 获取yaml文件数据
+current_path = os.path.abspath(".")
+yaml_path = os.path.join(current_path, "keyboard.yaml")
+def getConfig():
+    file = open(yaml_path, 'r', encoding="utf-8")
+    file_data = file.read()
+    file.close()
+    data = yaml.full_load(file_data)
+    return data
 
 # 调用服务
-def call_service(domain, service, data):
+def call_service(hass_token, domain, service, data):
     url = 'http://localhost:8123/api/services/'+ domain +'/'+ service
     headers = {
-        'Authorization': 'Bearer 这里是密钥',
+        'Authorization': 'Bearer ' + hass_token,
         'content-type': 'application/json',
     }
-
     response = post(url, json.dumps(data), headers=headers)
     print(response.text)
 
@@ -35,44 +48,26 @@ def detectInputKey():
         select([dev], [], [])
         for event in dev.read():
             if (event.value == 1 or event.value == 0) and event.code != 0:
+                print(event.code, event.value)
                 if (event.value == 1):
                     print('按下')
                 else:
                     print('放开')
-                    if (event.code == 71):
-                        # 7
-                        call_service('light', 'toggle', { 'entity_id': 'light.xiaomi_philips_light'})
-                    elif (event.code == 72):
-                        # 8
-                        call_service('light', 'toggle', { 'entity_id': 'light.cai_deng'})
-                    elif (event.code == 73):
-                        # 9
-                        print('not')
-                    elif (event.code == 74):
-                        # -
-                        call_service('media_player', 'volume_down', { 'entity_id': 'media_player.ha_cloud_music'})
-                    elif (event.code == 75):
-                        # 4
-                        call_service('switch', 'toggle', { 'entity_id': 'switch.dian_shi'})
-                    elif (event.code == 76):
-                        # 5
-                        call_service('switch', 'toggle', { 'entity_id': 'switch.tian_mao_he_zi'})
-                    elif (event.code == 77):
-                        # 6    
-                        print('6')
-                    elif (event.code == 78):
-                        # +
-                        call_service('media_player', 'volume_up', { 'entity_id': 'media_player.ha_cloud_music'})
-                    elif (event.code == 79):
-                        # 1
-                        call_service('media_player', 'media_previous_track', { 'entity_id': 'media_player.ha_cloud_music'})
-                    elif (event.code == 80):
-                        # 2
-                        call_service('media_player', 'media_play_pause', { 'entity_id': 'media_player.ha_cloud_music'})
-                    elif (event.code == 81):
-                        # 3
-                        call_service('media_player', 'media_next_track', { 'entity_id': 'media_player.ha_cloud_music'})
-                print(event.code, event.value)
+                    # 获取配置文件
+                    cfg = getConfig()
+                    _token = cfg['token']
+                    _code = event.code
+                    # 如果当前code定义
+                    if _code in cfg['key']:
+                        _result = cfg['key'][_code]
+                        # 如果有定义服务，则调用
+                        if _result != None:
+                            _sv = _result['service'].split('.')
+                            _domain = _sv[0]
+                            _service = _sv[1]
+                            call_service(_token, _domain, _service, _result['data'])
+                    else:
+                        print('当前未定义处理事件')
 
 if __name__ == '__main__':
     detectInputKey()
