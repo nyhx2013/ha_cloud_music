d90716af92086d485fd6d80f978b93b997cebe18
zhulu <shaonianzhentan@sina.cn>
2019-11-26 20:34:04 +0800

    update

--- public/data/ha-panel-baidu-map.js
+++ public/data/ha-panel-baidu-map.js
@@ -26,11 +26,11 @@ class HaPanelBaiduMap extends HTMLElement {
   
   ready(){
       if(window.BMap){
-          console.log(window.BMap)
-          console.log('%O',this)
             // 添加标题
             let toolbar = this.shadow.querySelector('app-toolbar')
             if(toolbar.children.length === 0){
+                  console.log(window.BMap)
+                  console.log('%O',this)
                   console.log('%O',toolbar)              
                     let menuButton = document.createElement('ha-menu-button')
                     menuButton.hass = this.hass
@@ -41,41 +41,169 @@ class HaPanelBaiduMap extends HTMLElement {
                     title.setAttribute('main-title','')
                     title.textContent = '百度地图'
                     toolbar.appendChild(title)
+                    // 获取当前HA配置的经纬度
+                    let {latitude, longitude} = this.hass.config
+                    // 生成一个对象
+                    var map = new BMap.Map(this.shadow.querySelector('#baidu-map'));
+                    this.map = map                      
+                    //添加地图类型控件
+                    map.addControl(new BMap.MapTypeControl({
+                      mapTypes:[
+                          BMAP_NORMAL_MAP,
+                          BMAP_HYBRID_MAP
+                    ]}))
+                    // 启用鼠标滚轮缩放
+                    map.enableScrollWheelZoom();
                     
-                    var mp = new BMap.Map(this.shadow.querySelector('#baidu-map'));
-                    mp.centerAndZoom(new BMap.Point(116.3964, 39.9093), 10);
-                    mp.enableScrollWheelZoom();                      
-                    var canvasLayer = new BMap.CanvasLayer({
-                      update: update
-                    });
-
-                    function update() {
-                      var ctx = this.canvas.getContext("2d");
-
-                      if (!ctx) {
-                        return;
-                      }
-
-                      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-                      var temp = {};
-                      ctx.fillStyle = "rgba(50, 50, 255, 0.7)";
-                      ctx.beginPath();
-                      var data = [
-                        new BMap.Point(116.297047, 39.979542),
-                        new BMap.Point(116.321768, 39.88748),
-                        new BMap.Point(116.494243, 39.956539)
-                      ];
-
-                      for (var i = 0, len = data.length; i < len; i++) {
-                        var pixel = mp.pointToPixel(data[i]);
-                        ctx.fillRect(pixel.x, pixel.y, 30, 30);
-                      }
-                    }
-                    mp.addOverlay(canvasLayer);
-            }              
+                      this.actionPanel()
+                    
+                      this.translate({longitude, latitude}).then(res=>{
+                          // 添加圆形区域
+                          let mPoint = res[0]
+                          var circle = new BMap.Circle(mPoint,100,{fillColor:"blue", strokeWeight: 1 ,fillOpacity: 0.3, strokeOpacity: 0.3});
+                          map.addOverlay(circle);
+                          // 添加家坐标
+                          this.addOverlay({latitude: mPoint.lat, longitude: mPoint.lng, pic: 'https://api.jiluxinqing.com/api/service/cdn/home.png'}, 'zone')
+                          // 中心点
+                          map.centerAndZoom(mPoint, 18);
+                      })
+                    
+                    this.update()
+                    /*
+                    var top_left_control = new BMap.ScaleControl({anchor: BMAP_ANCHOR_TOP_LEFT});// 左上角，添加比例尺
+                    var top_left_navigation = new BMap.NavigationControl();  //左上角，添加默认缩放平移控件
+                    var top_right_navigation = new BMap.NavigationControl({anchor: BMAP_ANCHOR_TOP_RIGHT, type: BMAP_NAVIGATION_CONTROL_SMALL}); //右上角，仅包含平移和缩放按钮
+                    mp.addControl(top_left_control);        
+                    mp.addControl(top_left_navigation);     
+                    mp.addControl(top_right_navigation);    
+                    */
+            }else{
+              this.update()
+            }
       }        
   }
+  
+  // 设置定位
+  addOverlay({latitude, longitude, pic}, type, click){
+      let mPoint = new BMap.Point(longitude, latitude)
+      //创建家庭图标
+      var myIcon = new BMap.Icon(pic, new BMap.Size(48,48));
+      var marker = new BMap.Marker(mPoint, {icon:myIcon});  // 创建标注        
+      marker.type = type
+      marker.addEventListener("click",function(){
+          if(typeof click === 'function'){
+              click(this)
+          }            
+      });
+      this.map.addOverlay(marker);              // 将标注添加到地图中
+  }
+  
+  // 更新位置
+  update(){
+      let map = this.map
+      // 删除所有设备
+      var allOverlay = map.getOverlays();
+      if(allOverlay.length>0){
+          for (var i = 0; i < allOverlay.length -1; i++){
+              if(allOverlay[i].type === "device"){
+                  map.removeOverlay(allOverlay[i]);
+              }
+          }
+      }
+      
+      
+      // 这里添加设备
+      let states = this.hass.states
+      let keys = Object.keys(states).filter(ele=>ele.indexOf('device_tracker') === 0)
+      keys.forEach(key => {
+          let stateObj = states[key]
+          let attr = stateObj.attributes
+          if('longitude' in attr && 'latitude' in attr){
+              let pic = 'https://api.jiluxinqing.com/api/service/cdn/people.png'
+              if('entity_picture' in attr){
+                  pic = attr.entity_picture
+              }
+              this.translate({longitude: attr.longitude, latitude: attr.latitude}).then(res=>{
+                  let point = res[0]
+                  this.addOverlay({latitude: point.lat, longitude: point.lng, pic}, 'device' , ()=>{
+                       // this.fire("hass-notification", { message:'这是一条测试数据' });
+                      this.fire('hass-more-info',  { entityId: key })
+                  })
+              })                
+          }
+      })
+  }
+  
+  // 坐标转换
+  translate({longitude, latitude}){
+      return new Promise((resolve,reject)=>{
+          var points = [new BMap.Point(longitude, latitude)]
+          var convertor = new BMap.Convertor();
+          convertor.translate(points, 1, 5, function(data){
+              if(data.status === 0){
+                 resolve(data.points)
+              }                
+          })    
+      })        
+  }
+  
+  // 触发事件
+  fire(type,data){
+      const event = new Event(type, {
+          bubbles: true,
+          cancelable: false,
+          composed: true
+        });
+      event.detail = data;
+      this.dispatchEvent(event);
+  }
+  
+  // 操作面板
+  actionPanel(){
+      const map = this.map
+      // 定义一个控件类,即function
+      function ZoomControl(){
+        // 默认停靠位置和偏移量
+        this.defaultAnchor = BMAP_ANCHOR_TOP_LEFT;
+        this.defaultOffset = new BMap.Size(10, 10);
+      }
+
+      // 通过JavaScript的prototype属性继承于BMap.Control
+      ZoomControl.prototype = new BMap.Control();
+
+      // 自定义控件必须实现自己的initialize方法,并且将控件的DOM元素返回
+      // 在本方法中创建个div元素作为控件的容器,并将其添加到地图容器中
+      ZoomControl.prototype.initialize = function(map){
+        // 创建一个DOM元素
+        var div = document.createElement("div");
+        let select = document.createElement('select')
+        select.innerHTML = `
+          <option>我</option>
+          <option>你</option>
+          <option>家</option>
+        `
+        select.onchange=function(){
+            // 这里重新定位
+            console.log(this)
+            // map.centerAndZoom(new BMap.Point(135, 34), 10);
+        }
+        // 添加文字说明
+        div.appendChild(select);
+        // 添加DOM元素到地图中
+        map.getContainer().appendChild(div);
+        // 将DOM元素返回
+        return div;
+      }
+      // 创建控件
+      var myZoomCtrl = new ZoomControl();
+      // 添加到地图当中
+      map.addControl(myZoomCtrl);
+      
+  }
+  
+  
+  
+  
   
   loadScript(src){
       return new Promise((resolve, reject) => {
