d7bc966f917a705c492ed0657071214a0c4e61c7
zhulu <shaonianzhentan@sina.cn>
2020-11-26 11:03:54 +0800

    优化识别

--- custom_components/ha_cloud_music/api_voice.py
+++ custom_components/ha_cloud_music/api_voice.py
@@ -1,4 +1,6 @@
 import re
+from .util import format_number, matcher_singer_music, matcher_playlist_index, \
+    matcher_play_pause, matcher_prev_next, matcher_play_music, matcher_volume_setting
 
 class ApiVoice():
 
@@ -6,44 +8,26 @@ class ApiVoice():
         self.hass = hass
         self.api_music = api_music
 
+    async def call_service(self, action, data = {}):
+        data.update({ 'entity_id': 'media_player.yun_yin_le' })
+        await hass.services.async_call('media_player', action, data)
+
+
     async def text_event(self, event):
         hass = self.hass
-        _text = event.data.get('text', '').strip(' 。，、＇：∶；?‘’“”〝〞ˆˇ﹕︰﹔﹖﹑·¨….¸;！´？！～—ˉ｜‖＂〃｀@﹫¡¿﹏﹋﹌︴々﹟#﹩$﹠&﹪%*﹡﹢﹦﹤‐￣¯―﹨ˆ˜﹍﹎+=<­­＿_-\ˇ~﹉﹊（）〈〉‹›﹛﹜『』〖〗［］《》〔〕{}「」【】︵︷︿︹︽_﹁﹃︻︶︸﹀︺︾ˉ﹂﹄︼')
-        #_log_info('监听语音小助手的文本：' + _text)
-        # 我想听xxx的歌
-        pattern = re.compile(r"^我想听(.+)的歌$")
-        singer = pattern.findall(_text)
-        if len(singer) == 1:
-            # 正在播放xxx的歌
-            singerName = singer[0]
-            # 开始搜索当前歌手的热门歌曲
-            await self.api_music.play_singer_hotsong(singerName)
+        text = event.data.get('text', '').strip(' 。，、＇：∶；?‘’“”〝〞ˆˇ﹕︰﹔﹖﹑·¨….¸;！´？！～—ˉ｜‖＂〃｀@﹫¡¿﹏﹋﹌︴々﹟#﹩$﹠&﹪%*﹡﹢﹦﹤‐￣¯―﹨ˆ˜﹍﹎+=<­­＿_-\ˇ~﹉﹊（）〈〉‹›﹛﹜『』〖〗［］《》〔〕{}「」【】︵︷︿︹︽_﹁﹃︻︶︸﹀︺︾ˉ﹂﹄︼')
+        # 精准匹配
+        if ['声音小点', '小点声音', '小一点声音', '声音小一点'].count(text) == 1:
+            await self.call_service('volume_down')
             # 触发语音事件
             hass.bus.fire("ha_cloud_music_voice_event", {})
-        # 播放第几集
-        pattern = re.compile(r"^播放第(.+)[集|首]$")
-        NO = pattern.findall(_text)
-        if len(NO) == 1:
-            index = NO[0]
-            if is_number(index) == False:
-                index = t(index)
-            music_playlist = self.api_music.media.music_playlist
-            if music_playlist is not None and len(music_playlist) > 0:
-                music_info = music_playlist[0]
-                # 获取加载数据
-                if 'load' in music_info:
-                    load = music_info['load']
-                    print(load)
-                    # 播放推荐音乐
-                    await hass.services.async_call('ha_cloud_music', 'load', {
-                            'id': load['id'], 
-                            'type': load['type'], 
-                            'index': index
-                        })
-                    # 触发语音事件
-                    hass.bus.fire("ha_cloud_music_voice_event", {})
-        # 重新加载专辑
-        if _text == '重新加载专辑':
+            return
+        elif ['声音大点', '大点声音', '大一点声音', '声音大一点'].count(text) == 1:
+            await self.call_service('volume_up')
+            # 触发语音事件
+            hass.bus.fire("ha_cloud_music_voice_event", {})
+            return
+        elif text == '重新加载专辑':
             music_playlist = self.api_music.media.music_playlist
             if music_playlist is not None and len(music_playlist) > 0:
                 music_info = music_playlist[0]
@@ -57,147 +41,92 @@ class ApiVoice():
                             'list': _newlist
                         })
                         print('重新加载专辑成功')
-
-        # 播放电台 xxxx
-        if _text.find('播放电台') == 0:
-            _name = _text.split('播放电台')[1]
-            await self.api_music.play_dj_hotsong(_name)
-            # 触发语音事件
-            hass.bus.fire("ha_cloud_music_voice_event", {})
-        # 播放歌单 xxxx
-        if _text.find('播放歌单') == 0:
-            _name = _text.split('播放歌单')[1]
-            await self.api_music.play_list_hotsong(_name)
-            # 触发语音事件
-            hass.bus.fire("ha_cloud_music_voice_event", {})
-        # 播放歌曲 xxxx
-        if _text.find('播放歌曲') == 0:
-            _name = _text.split('播放歌曲')[1]
-            await self.api_music.play_song(_name)
-            # 触发语音事件
-            hass.bus.fire("ha_cloud_music_voice_event", {})
-        # 播放新闻
-        if _text.find('播放新闻') == 0:
-            _name = _text.split('播放新闻')[1]
-            await self.api_music.play_news(_name)
-            # 触发语音事件
-            hass.bus.fire("ha_cloud_music_voice_event", {})
-        # 播放专辑
-        if _text.find('播放专辑') == 0:
-            _name = _text.split('播放专辑')[1]
-            # 这里配置是否还有指定集数
-            pattern = re.compile(r"(.+)第(.+)[集|首]$")
-            NO = pattern.findall(_name)
-            if len(NO) == 1:
-                _name = NO[0][0]
-                index = NO[0][1]
-                if is_number(index) == False:
-                    index = t(index)
-                print(_name, index)
-                await self.api_music.play_ximalaya(_name, int(index))
+                        return
+        
+        # (播放|暂停)音乐
+        result = matcher_play_pause(text)
+        if result is not None:
+            if result == '播放':
+                self.api_music.media.media_play()
             else:
-                # 优先读取本地数据
-                await self.api_music.play_ximalaya(_name, -1)
+                self.api_music.media.media_pause()
             # 触发语音事件
             hass.bus.fire("ha_cloud_music_voice_event", {})
-        # 播放广播
-        if _text.find('播放广播') == 0:
-            _name = _text.split('播放广播')[1]
-            await self.api_music.play_fm(_name)
+            return
+        
+        # 上一曲、下一曲
+        result = matcher_prev_next(text)
+        if result is not None:
+            if ['上','前'].count(result) == 1:
+                self.api_music.media.media_previous_track()
+            else:
+                self.api_music.media.media_next_track()
             # 触发语音事件
             hass.bus.fire("ha_cloud_music_voice_event", {})
+            return
 
-        # 音乐控制解析
-        if '下一曲' == _text:
-            self.api_music.media.media_next_track()
+        # 播放歌手的音乐
+        result = matcher_singer_music(text)
+        if result is not None:
+            # 开始搜索当前歌手的热门歌曲
+            await self.api_music.play_singer_hotsong(result)            
             # 触发语音事件
             hass.bus.fire("ha_cloud_music_voice_event", {})
-        elif '上一曲' == _text:
-            self.api_music.media.media_previous_track()
-            # 触发语音事件
-            hass.bus.fire("ha_cloud_music_voice_event", {})
-        elif '播放音乐' == _text:
-            self.api_music.media.media_play()
-            # 触发语音事件
-            hass.bus.fire("ha_cloud_music_voice_event", {})
-        elif '暂停音乐' == _text:
-            self.api_music.media.media_pause()
-            # 触发语音事件
-            hass.bus.fire("ha_cloud_music_voice_event", {})
-        elif '声音小点' == _text or '小点声音' == _text or '小一点声音' == _text or '声音小一点' == _text:
-            await hass.services.async_call('media_player', 'volume_down', {'entity_id': 'media_player.yun_yin_le'})
-            # 触发语音事件
-            hass.bus.fire("ha_cloud_music_voice_event", {})
-        elif '声音大点' == _text or '大点声音' == _text or '大一点声音' == _text or '声音大一点' == _text:
-            await hass.services.async_call('media_player', 'volume_up', {'entity_id': 'media_player.yun_yin_le'})
-            # 触发语音事件
-            hass.bus.fire("ha_cloud_music_voice_event", {})
+            return
+        
+        # 播放第几集
+        result = matcher_playlist_index(text)
+        if result is not None:
+            music_playlist = self.api_music.media.music_playlist
+            if music_playlist is not None and len(music_playlist) > 0:
+                music_info = music_playlist[0]
+                # 获取加载数据
+                if 'load' in music_info:
+                    load = music_info['load']
+                    # 播放推荐音乐
+                    await hass.services.async_call('ha_cloud_music', 'load', {
+                            'id': load['id'], 
+                            'type': load['type'], 
+                            'index': result
+                        })
+                    # 触发语音事件
+                    hass.bus.fire("ha_cloud_music_voice_event", {})
+                    return
 
-# 判断是否数字
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        pass
- 
-    try:
-        import unicodedata
-        unicodedata.numeric(s)
-        return True
-    except (TypeError, ValueError):
-        pass
- 
-    return False
+        # 播放(电台|歌单|歌曲|新闻|广播|专辑)(.*)
+        result = matcher_singer_music(text)
+        if result is not None:
+            action = result[0]
+            _name = result[1]
+            if action == '电台':
+                await self.api_music.play_dj_hotsong(_name)
+            elif action == '歌单':
+                await self.api_music.play_list_hotsong(_name)
+            elif action == '歌曲':
+                await self.api_music.play_song(_name)
+            elif action == '新闻':
+                await self.api_music.play_news(_name)
+            elif action == '广播':
+                await self.api_music.play_fm(_name)
+            elif action == '专辑':
+                # 这里配置是否还有指定集数
+                pattern = re.compile(r"(.+)第(.+)[集|首]$")
+                NO = pattern.findall(_name)
+                if len(NO) == 1:
+                    _name = NO[0][0]
+                    index = format_number(NO[0][1])
+                    print(_name, index)
+                    await self.api_music.play_ximalaya(_name, int(index))
+                else:
+                    # 优先读取本地数据
+                    await self.api_music.play_ximalaya(_name, -1)            
+            hass.bus.fire("ha_cloud_music_voice_event", {})
+            return
 
-# 汉字转数字
-def t(str):
-    zhong={'零':0,'一':1,'二':2,'三':3,'四':4,'五':5,'六':6,'七':7,'八':8,'九':9}
-    danwei={'十':10,'百':100,'千':1000,'万':10000}
-    num=0
-    if len(str)==0:
-        return 0
-    if len(str)==1:
-        if str == '十':
-            return 10
-        num=zhong[str]
-        return num
-    temp=0
-    if str[0] == '十':
-        num=10
-    for i in str:
-        if i == '零':
-            temp=zhong[i]
-        elif i == '一':
-            temp=zhong[i]
-        elif i == '二':
-            temp=zhong[i]
-        elif i == '三':
-            temp=zhong[i]
-        elif i == '四':
-            temp=zhong[i]
-        elif i == '五':
-            temp=zhong[i]
-        elif i == '六':
-            temp=zhong[i]
-        elif i == '七':
-            temp=zhong[i]
-        elif i == '八':
-            temp=zhong[i]
-        elif i == '九':
-            temp=zhong[i]
-        if i == '十':
-            temp=temp*danwei[i]
-            num+=temp
-        elif i == '百':
-            temp=temp*danwei[i]
-            num+=temp
-        elif i == '千':
-            temp=temp*danwei[i]
-            num+=temp
-        elif i == '万':
-            temp=temp*danwei[i]
-            num+=temp
-    if str[len(str)-1] != '十'and str[len(str)-1] != '百'and str[len(str)-1] != '千'and str[len(str)-1] != '万':
-        num+=temp
-    return num
+        # 音量调整
+        result = matcher_volume_setting(text)
+        if result is not None:            
+            await self.call_service('volume_set', { 'volume_level': result[1] })
+            # 触发语音事件
+            hass.bus.fire("ha_cloud_music_voice_event", {})
+            return
---
+++ custom_components/ha_cloud_music/util.py
@@ -0,0 +1,107 @@
+"""Util for Conversation."""
+import re
+import string
+import random
+
+########################################## 常量
+
+########################################## 查询实体
+
+########################################## 汉字转数字
+common_used_numerals_tmp ={'零':0, '一':1, '二':2, '两':2, '三':3, '四':4, '五':5, '六':6, '七':7, '八':8, '九':9, '十':10, '百':100, '千':1000, '万':10000, '亿':100000000}
+common_used_numerals = {}
+for key in common_used_numerals_tmp:
+  common_used_numerals[key.encode('cp936').decode('cp936')] = common_used_numerals_tmp[key]
+
+def chinese2digits(uchars_chinese):
+    try:
+        uchars_chinese = uchars_chinese.encode('cp936').decode('cp936')
+        total = 0
+        r = 1              #表示单位：个十百千...
+        for i in range(len(uchars_chinese) - 1, -1, -1):
+            val = common_used_numerals.get(uchars_chinese[i])
+            if val >= 10 and i == 0:  #应对 十三 十四 十*之类
+                if val > r:
+                    r = val
+                    total = total + val
+                else:
+                    r = r * val
+                #total =total + r * x
+            elif val >= 10:
+                if val > r:
+                    r = val
+                else:
+                    r = r * val
+            else:
+                total = total + r * val
+        return total
+    except Exception as ex:
+        return None
+
+# 判断是否数字
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+ 
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+ 
+    return False
+
+def format_number(num):
+    if is_number(num) == False:
+        num = chinese2digits(num)
+    return float(num)
+
+########################################## (我想听|播放)(.+)的(歌|音乐)
+def matcher_singer_music(text):
+    matchObj = re.match(r'(我想听|播放)(.+)的(歌|音乐)', text)
+    if matchObj is not None:
+        return matchObj.group(2)
+
+########################################## 播放(电台|歌单|歌曲|新闻|广播|专辑)(.*)
+def matcher_play_music(text):
+    matchObj = re.match(r'播放(电台|歌单|歌曲|新闻|广播|专辑)(.*)', text)
+    if matchObj is not None:
+        return (matchObj.group(1), matchObj.group(2))
+
+########################################## (播放|暂停)音乐
+def matcher_play_pause(text):
+    matchObj = re.match(r'(播放|暂停)音乐', text)
+    if matchObj is not None:
+        return matchObj.group(1)
+
+########################################## (上|下|前|后)一(曲|首)
+def matcher_prev_next(text):
+    matchObj = re.match(r'(上|下|前|后)一(曲|首)', text)
+    if matchObj is not None:
+        return matchObj.group(1)
+
+########################################## 集数调整
+def matcher_playlist_index(text):
+    matchObj = re.match(r'播放第(.+)(集|首)(.*)', text)
+    if matchObj is not None:
+        index = matchObj.group(1)
+        if is_number(index) == False:
+            index = chinese2digits(index)
+        return int(index)
+
+########################################## 音量调整
+def matcher_volume_setting(text):
+    matchObj = re.match(r'把(TTS|音乐)(声音|音量)调到(.+)', text)
+    if matchObj is not None:
+        volume_level = matchObj.group(3)
+        if volume_level == '最大':
+            volume_level = 100.0
+        elif volume_level == '最小':
+            volume_level = 20.0
+        elif is_number(volume_level) == False:
+            volume_level = chinese2digits(volume_level)
+        return (matchObj.group(1), float(volume_level) / 100.0)
